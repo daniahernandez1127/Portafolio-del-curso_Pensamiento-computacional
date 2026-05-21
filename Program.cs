dc9cbb4faeb989119e41771e6289bd9999a15e13
daniahernandez1127 <dghernandezta@correo.url.edu.gt>
2026-05-20 18:45:28 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Este es un comentario - será ignorado en tiempo de ejecución
+Console.WriteLine("==========================================");
+Console.WriteLine("Laboratorio # 03");
+// Ingresar aquí nombre y carnet del dueño del repositorio
+Console.WriteLine("Nombre: Juan Carlos Alarcon Amado Carnet: 1227526");
+// Ingresar aquí nombre y carnet del colaborador:
+
+Console.WriteLine("==========================================");
+Console.ReadLine();
+// Ingresar aquí nombre y carnet del colaborador
+Console.WriteLine("Nombre: Dania {gabriela Hernandez Tavic, Carnet: 1152026");
+
